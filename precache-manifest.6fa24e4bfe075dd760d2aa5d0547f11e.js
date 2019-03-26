ab048a18a598748bc913633f391d6f1331b2b590
A.J. Bale <abalejr@gmail.com>
2019-03-26 00:27:43 -0500

    Updates

---
+++ precache-manifest.6fa24e4bfe075dd760d2aa5d0547f11e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "404d150a092d555494d9",
+    "url": "/newsnstuff/static/js/runtime~main.e510688a.js"
+  },
+  {
+    "revision": "13fa421c6bbbb4b0fafe",
+    "url": "/newsnstuff/static/js/main.408dc01a.chunk.js"
+  },
+  {
+    "revision": "8bd72f33243c8d17df27",
+    "url": "/newsnstuff/static/js/2.eb25a4b4.chunk.js"
+  },
+  {
+    "revision": "13fa421c6bbbb4b0fafe",
+    "url": "/newsnstuff/static/css/main.4db0ebeb.chunk.css"
+  },
+  {
+    "revision": "a4a253381de772f0140ab59ec114595d",
+    "url": "/newsnstuff/index.html"
+  }
+];
